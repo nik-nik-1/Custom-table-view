0761652a4214d0b90183a255dd0487c87eba81a7
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-06-18 17:42:24 +0300

    new header in building...

--- Custom-table-view/Custom-table-view/Table/Header/TableSectionHeader.swift
+++ Custom-table-view/Custom-table-view/Table/Header/TableSectionHeader.swift
@@ -8,50 +8,100 @@
 
 import UIKit
 
-
-class TableSectionHeader: UITableViewHeaderFooterView {
+@IBDesignable
+class TableSectionHeader: UIView {//UITableViewHeaderFooterView
+//{
     
-    @IBOutlet weak var headerLabel: UILabel!
-    @IBOutlet weak var conView: UIView!
+    // Our custom view from the XIB file
+    var view: UIView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    // Outlets
+    @IBOutlet weak var label1: UILabel!
+    //@IBOutlet weak var conView: UIView!
     
-    
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
+    override init(frame: CGRect) {
+        // 1. setup any properties here
         
-        layoutSubviews()
+        // 2. call super.init(frame:)
+        super.init(frame: frame)
+        
+        // 3. Setup view from .xib file
+        xibSetup()
     }
     
     required init?(coder aDecoder: NSCoder) {
-        //        // 1. setup any properties here
-        //
-        //        // 2. call super.init(coder:)
+        // 1. setup any properties here
+        
+        // 2. call super.init(coder:)
         super.init(coder: aDecoder)
-        //
-        //        // 3. Setup view from .xib file
         
-        layoutSubviews()
+        // 3. Setup view from .xib file
+        xibSetup()
     }
     
-    override func layoutSubviews() {
+    func xibSetup() {
+        view = loadViewFromNib()
         
-        super.layoutSubviews()
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
         
-        conView?.backgroundColor = UIColor(hex: "#FF9090",alpha: 0.8)
-        headerLabel?.textColor = UIColor.blackColor()
-        alpha = 0.5
+        // Make the view stretch with containing view
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
         
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
     }
     
-    /*
-     // Only override drawRect: if you perform custom drawing.
-     // An empty implementation adversely affects performance during animation.
-     override func drawRect(rect: CGRect) {
-     // Drawing code
-     }
-     */
+    func loadViewFromNib() -> UIView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "View.TableSectionHeader", bundle: bundle)
+        
+        // Assumes UIView is top level and only object in CustomView.xib file
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        return view
+    }
+    
+    
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//    
+//    
+//    override init(reuseIdentifier: String?) {
+//        super.init(reuseIdentifier: reuseIdentifier)
+//        
+//        layoutSubviews()
+//    }
+//    
+//    required init?(coder aDecoder: NSCoder) {
+//        //        // 1. setup any properties here
+//        //
+//        //        // 2. call super.init(coder:)
+//        super.init(coder: aDecoder)
+//        //
+//        //        // 3. Setup view from .xib file
+//        
+//        layoutSubviews()
+//    }
+//    
+//    override func layoutSubviews() {
+//        
+//        super.layoutSubviews()
+//        
+//        conView?.backgroundColor = UIColor(hex: "#FF9090",alpha: 0.8)
+//        headerLabel?.textColor = UIColor.blackColor()
+//        alpha = 0.5
+//        
+//    }
+//    
+//    /*
+//     // Only override drawRect: if you perform custom drawing.
+//     // An empty implementation adversely affects performance during animation.
+//     override func drawRect(rect: CGRect) {
+//     // Drawing code
+//     }
+//     */
 }
---
+++ Custom-table-view/Custom-table-view/Table/TableCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  TableCustomView.swift
+//  Custom-table-view
+//
+//  Created by mc373 on 18.06.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+import UIKit
+
+class TableCustomView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
